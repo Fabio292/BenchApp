29f8268e9850423223116530444ef21614475acc
FabioG <fab.gentile@hotmail.it>
2016-07-21 12:59:14 +0200

    First commit

---
+++ app/src/main/java/fabiogentile/benchapp/BenchMain.java
@@ -0,0 +1,13 @@
+package fabiogentile.benchapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BenchMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bench_main);
+    }
+}
